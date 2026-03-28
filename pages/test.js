aef8876a064c6c073b173bcb1758f08568c0a55d
svcunhamg69 <saulo.cunha69@gmail.com>
2026-03-28 11:14:13 -0300

    Refactor TestPage to Test component

--- pages/test.js
+++ pages/test.js
@@ -1,18 +1,3 @@
-import { supabase } from '../lib/supabase'
-
-export default function TestPage() {
-  async function testar() {
-    const { data, error } = await supabase
-      .from('teste')
-      .select('*')
-
-    console.log(data, error)
-  }
-
-  return (
-    <div>
-      <h1>Teste Supabase</h1>
-      <button onClick={testar}>Testar conexão</button>
-    </div>
-  )
+export default function Test() {
+  return <div>Página de teste</div>
 }
